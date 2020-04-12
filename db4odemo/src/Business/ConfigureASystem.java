62e8590bb08761d63057391b565b13a0a04e0253
yenhuahuang <huang.yenh@husky.neu.edu>
2020-04-12 12:46:08 +0800

    confugure file edit, network list

--- db4odemo/src/Business/ConfigureASystem.java
+++ db4odemo/src/Business/ConfigureASystem.java
@@ -39,26 +39,27 @@ public class ConfigureASystem {
         
         system.getCountSymtoms().setCountAndType();
         //Create a network
-        ArrayList<String> states=new ArrayList();
-   
-        states.add("Alabama");states.add("Alaska");states.add("Arizona");
-        states.add("Arkansas");states.add("Alabama");states.add("California");
-        states.add("Colorado");states.add("Connecticut");states.add("Delaware");
-        states.add("Florida");states.add("Georgia");states.add("Hawaii");
-        states.add("Idaho");  states.add("Illinois");states.add("Indiana");states.add("Iowa");
-        states.add( "Kansas");  states.add("Kentucky");  states.add("Louisiana");  states.add("Maine");  states.add("Maryland");
-        states.add("Michigan");  states.add("Minnesota");  states.add("Mississippi");  states.add("Missouri");
-        states.add("Montana");  states.add("Nebraska");  states.add("Nevada");  states.add("New Hampshire");  states.add("New Jersey");
-        states.add("New Mexico");  states.add("New York");  states.add("North Carolina");  states.add("North Dakota");  states.add("New Jersey");
-        states.add("Ohio");  states.add("Oklahoma");  states.add("Oregon");  states.add("Pennsylvania");  states.add("Rhode Island");
-        states.add("South Carolina");  states.add("South Dakota");  states.add("Tennessee");  states.add("Texas");  states.add("Utah");
-        states.add("Vermont");  states.add("Virginia");  states.add("Washington");  states.add("West Virginia");  states.add("Wisconsin");
-        states.add("Wyoming");  
-        for(String r:states){
-            Network network=new Network();
-            network.setName(r);
-            system.getNetworkList().add(network);
-            }
+//        ArrayList<String> states=new ArrayList();
+//   
+//        states.add("Alabama");states.add("Alaska");states.add("Arizona");
+//        states.add("Arkansas");states.add("Alabama");states.add("California");
+//        states.add("Colorado");states.add("Connecticut");states.add("Delaware");
+//        states.add("Florida");states.add("Georgia");states.add("Hawaii");
+//        states.add("Idaho");  states.add("Illinois");states.add("Indiana");states.add("Iowa");
+//        states.add( "Kansas");  states.add("Kentucky");  states.add("Louisiana");  states.add("Maine");  states.add("Maryland");
+//        states.add("Massachusetts");
+//        states.add("Michigan");  states.add("Minnesota");  states.add("Mississippi");  states.add("Missouri");
+//        states.add("Montana");  states.add("Nebraska");  states.add("Nevada");  states.add("New Hampshire");  states.add("New Jersey");
+//        states.add("New Mexico");  states.add("New York");  states.add("North Carolina");  states.add("North Dakota");  states.add("New Jersey");
+//        states.add("Ohio");  states.add("Oklahoma");  states.add("Oregon");  states.add("Pennsylvania");  states.add("Rhode Island");
+//        states.add("South Carolina");  states.add("South Dakota");  states.add("Tennessee");  states.add("Texas");  states.add("Utah");
+//        states.add("Vermont");  states.add("Virginia");  states.add("Washington");  states.add("West Virginia");  states.add("Wisconsin");
+//        states.add("Wyoming");  
+//        for(String r:states){
+//            Network network=new Network();
+//            network.setName(r);
+//            system.getNetworkList().add(network);
+//            }
         Network createNetwork=new Network();
         createNetwork.setName("Massachusetts");
         system.getNetworkList().add(createNetwork);
--- db4odemo/src/userinterface/SystemAdminWorkArea/ManageEnterpriseJPanel.java
+++ db4odemo/src/userinterface/SystemAdminWorkArea/ManageEnterpriseJPanel.java
@@ -30,34 +30,34 @@ public class ManageEnterpriseJPanel extends javax.swing.JPanel {
         initComponents();
         this.userProcessContainer = userProcessContainer;
         this.system = system;
-//ArrayList<String> states=new ArrayList();
-//   
-//        states.add("Alabama");  
-//          states.add("Alaska");
-//            states.add("Arizona");
-//              states.add("Arkansas");
-//                states.add("Alabama");
-//                  states.add("California");
-//                    states.add("Colorado");
-//                      states.add("Connecticut");
-//                        states.add("Delaware");
-//             states.add("Florida");
-//               states.add("Georgia");
-//                 states.add("Hawaii");
-//                   states.add("Idaho");  states.add("Illinois");states.add("Indiana");states.add("Iowa");
-//                   states.add( "Kansas");  states.add("Kentucky");  states.add("Louisiana");  states.add("Maine");  states.add("Maryland");
-//                      states.add("Massachusetts");  states.add("Michigan");  states.add("Minnesota");  states.add("Mississippi");  states.add("Missouri");
-//                         states.add("Montana");  states.add("Nebraska");  states.add("Nevada");  states.add("New Hampshire");  states.add("New Jersey");
-//         states.add("New Mexico");  states.add("New York");  states.add("North Carolina");  states.add("North Dakota");  states.add("New Jersey");
-//        states.add("Ohio");  states.add("Oklahoma");  states.add("Oregon");  states.add("Pennsylvania");  states.add("Rhode Island");
-//        states.add("South Carolina");  states.add("South Dakota");  states.add("Tennessee");  states.add("Texas");  states.add("Utah");
-//        states.add("Vermont");  states.add("Virginia");  states.add("Washington");  states.add("West Virginia");  states.add("Wisconsin");
-//        states.add("Wyoming");  
+ArrayList<String> states=new ArrayList();
+   
+        states.add("Alabama");  
+          states.add("Alaska");
+            states.add("Arizona");
+              states.add("Arkansas");
+                states.add("Alabama");
+                  states.add("California");
+                    states.add("Colorado");
+                      states.add("Connecticut");
+                        states.add("Delaware");
+             states.add("Florida");
+               states.add("Georgia");
+                 states.add("Hawaii");
+                   states.add("Idaho");  states.add("Illinois");states.add("Indiana");states.add("Iowa");
+                   states.add( "Kansas");  states.add("Kentucky");  states.add("Louisiana");  states.add("Maine");  states.add("Maryland");
+                        states.add("Michigan");  states.add("Minnesota");  states.add("Mississippi");  states.add("Missouri");
+                         states.add("Montana");  states.add("Nebraska");  states.add("Nevada");  states.add("New Hampshire");  states.add("New Jersey");
+         states.add("New Mexico");  states.add("New York");  states.add("North Carolina");  states.add("North Dakota");  states.add("New Jersey");
+        states.add("Ohio");  states.add("Oklahoma");  states.add("Oregon");  states.add("Pennsylvania");  states.add("Rhode Island");
+        states.add("South Carolina");  states.add("South Dakota");  states.add("Tennessee");  states.add("Texas");  states.add("Utah");
+        states.add("Vermont");  states.add("Virginia");  states.add("Washington");  states.add("West Virginia");  states.add("Wisconsin");
+        states.add("Wyoming");  
      
-//      for(String r:states){
-//      Network network=system.createAndAddNetwork();
-//      network.setName(r);
-//      }
+      for(String r:states){
+      Network network=system.createAndAddNetwork();
+      network.setName(r);
+      }
         populateTable();
         populateComboBox();
     }
